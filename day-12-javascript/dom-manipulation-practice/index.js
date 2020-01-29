63f3b1ae3c91bc13022e94d03270664885ef512d
Petr Bělecký <23419559+PetrBelecky@users.noreply.github.com>
2020-01-29 10:37:05 +0100

    added a first product section

--- day-12-javascript/dom-manipulation-practice/index.js
+++ day-12-javascript/dom-manipulation-practice/index.js
@@ -1,23 +1,37 @@
 
-//show alert
 const changeLabel = () => {
-  const btn = document.querySelector('.product__bnb');
-  const quantity = document.querySelector('.buy__input').value;
-  console.log(quantity);
+
+  // quantity in input field & message box
+  const quantity = Number(document.querySelector('.buy__input').value);
   const message = document.querySelector('.quantity__message');
 
+  const changeButton = () => {
+    const btn = document.querySelector('.product__bnb');
+    btn.textContent = 'In Cart';
+    btn.className = 'btn--cart';
+  }
+
+  const resetMessageColor = () => {
+    message.style.color = '#8fbc8f';
+    message.fontWeight = 'normal';
+  }
 
   if (quantity > 1) {
     message.textContent = 'You have ' + quantity + " Batducks in your cart."
+    resetMessageColor();
+    changeButton();
+
   } else if (quantity === 1) {
     message.textContent = 'You have ' + quantity + " Batduck in your cart."
+    resetMessageColor();
+    changeButton();
+
   } else {
     message.textContent = 'You have not entered any amount.'
     message.style.color = '#dd2626';
     message.style.fontWeight = 'bold';
   }
-
-  btn.textContent = 'In Cart';
-  btn.className = "btn--cart";
-
 }
+
+
+
