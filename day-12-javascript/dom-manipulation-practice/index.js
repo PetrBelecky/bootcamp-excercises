2a1f596d942e5645be220d230b77280b33b60e6f
Petr Bělecký <23419559+PetrBelecky@users.noreply.github.com>
2020-01-29 11:26:30 +0100

    added a login section

--- day-12-javascript/dom-manipulation-practice/index.js
+++ day-12-javascript/dom-manipulation-practice/index.js
@@ -33,5 +33,27 @@ const changeLabel = () => {
   }
 }
 
+const showMessage = (event) => {
+  event.preventDefault();
 
+  const passDoNotMatch = () => {
+    msg.textContent = 'Sorry, your passwords don\'t match.';
+    msg.className = 'msg msg--error';
+  }
 
+  const msgSuccess = () => {
+    msg.textContent = 'Your account was succesfully created.';
+    msg.className = 'msg msg--success';
+  }
+
+  const msg = document.querySelector('#message');
+  const pass1 = document.querySelector('#pass1').value;
+  const pass2 = document.querySelector('#pass2').value;
+
+  if (pass1 !== pass2) {
+    passDoNotMatch();
+  } else {
+    msgSuccess();
+  }
+
+}
