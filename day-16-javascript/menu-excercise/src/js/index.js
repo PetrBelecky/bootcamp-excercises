58fbd58b9ea734cabbd2d6aa324e163818f1dcd4
Petr Bělecký <23419559+PetrBelecky@users.noreply.github.com>
2020-02-08 17:48:45 +0100

    Rework according to instructor's solution

--- day-16-javascript/menu-excercise/src/js/index.js
+++ day-16-javascript/menu-excercise/src/js/index.js
@@ -1,12 +1,17 @@
 
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('connected');
 
+  const aside = document.querySelector('#aside');
 
+  aside.addEventListener('mouseenter', () => {
+    aside.classList.remove('aside--closed');
+  });
 
-
-
-
-
-
+  aside.addEventListener('mouseleave', () => {
+    aside.classList.add('aside--closed');
+  });
+})
 
 
 // previous solution
