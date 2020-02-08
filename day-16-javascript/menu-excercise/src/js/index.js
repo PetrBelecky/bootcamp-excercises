3d33340213bd37b09f78e620dd232230058f1edd
Petr Bělecký <23419559+PetrBelecky@users.noreply.github.com>
2020-02-08 17:49:46 +0100

    Delete commented-out code

--- day-16-javascript/menu-excercise/src/js/index.js
+++ day-16-javascript/menu-excercise/src/js/index.js
@@ -13,30 +13,3 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 })
 
-
-// previous solution
-
-/* document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM loaded");
-
-  const menuItemsEl = document.querySelector(".menu-wrapper");
-  const wrapperEl = document.querySelector("img");
-  const headline = document.querySelector("h1");
-
-  console.log(menuItemsEl);
-  console.log(wrapperEl);
-  console.log(headline);
-
-  wrapperEl.addEventListener("click", () => {
-    console.log("logo");
-
-    menuItemsEl.classList.toggle("hidden");
-    menuItemsEl.classList.toggle("active");
-  });
-
-  headline.addEventListener("click", () => {
-    console.log("entered");
-  });
-});
- */
-
