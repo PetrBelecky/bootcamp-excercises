02d08d2f58cba4a611ccb01b3dfa18d872f57f34
Petr Bělecký <23419559+PetrBelecky@users.noreply.github.com>
2020-03-19 13:02:52 +0100

    Add SWAPI exercise

--- day-42-react/swapi-exercise/src/App.jsx
+++ day-42-react/swapi-exercise/src/App.jsx
@@ -2,12 +2,94 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    // this state is container OBJECT
+    this.state = {
+      loading: true,
+      character: null,
+      characterFilmography: [],
+    }
+  }
+
+  fetchPersonAPI = async () => {
+    const randNumber = Math.ceil(Math.random() * 20);
+
+    try {
+      const response = await fetch(
+        `https://swapi.co/api/people/${randNumber}`
+      );
+
+      const person = await response.json();
+
+      this.setState({
+        character: {
+          name: person.name,
+          birthYear: person.birth_year,
+          homeworld: person.homeworld,
+        },
+      });
+
+      await this.fetchFilmAPI(person.films);
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  fetchFilmAPI = async (arrayOfFilms) => {
+
+    let films = [];
+
+    try {
+      arrayOfFilms.forEach(async (oneFilm) => {
+        const response = await fetch(oneFilm);
+        const film = await response.json();
+        films.push(film);
+      });
+
+      this.setState((previousState) => {
+        return {
+          ...previousState,
+          characterFilmography: films,
+          loading: false
+        }
+      })
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async componentDidMount() {
+    await this.fetchPersonAPI();
+  }
+
   render() {
+
+
     return (
-      <div className="App">
-        Start Here
-    </div>
-    );
+      <div>
+        {this.state.loading ?
+          'loading...' :
+          <>
+            <p>
+              <strong>Name: </strong>{this.state.character.name}
+            </p>
+            <p>
+              <strong>Birth: </strong>{this.state.character.birthYear}
+            </p>
+            <p>
+              <strong>Homeworld: </strong>{this.state.character.homeworld}
+            </p>
+            <p><strong>Films: </strong></p>
+          </>
+        }
+      </div>
+
+    )
   }
 }
 
